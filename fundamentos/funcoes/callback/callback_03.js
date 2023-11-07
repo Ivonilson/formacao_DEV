3373006a13f89e3c87b8b342c6c53814b86a9faa
Ivonilson <ivonilsoncardoso@hotmail.com>
2023-11-07 00:12:43 -0300

    update

--- fundamentos/funcoes/callback/callback_03.js
+++ fundamentos/funcoes/callback/callback_03.js
@@ -1,1 +1,24 @@
+const path = require('path')
+const fs = require('fs')
+
+const caminho = path.join(__dirname, '..', 'dados', 'aprovados.txt')
+
 console.log(__dirname + '/dados/aprovados.txt')
+
+function lerArquivo (erro, conteudo) {
+    if (erro) return
+    console.log(conteudo.toString())
+}
+
+/**execução assíncrona de leitura */
+fs.readFile(caminho, lerArquivo)
+console.log('fim #01...')
+
+/**execução síncrona de leitura */
+/*const conteudo = fs.readFileSync(caminho)
+console.log(conteudo.toString())
+console.log('fim #02...')*/
+
+
+
+
